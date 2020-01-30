6f0a6aa8e5dede6afb7f3feee160d23b60957224
Ragnor Comerford <hello@ragnor.co>
2020-01-30 12:46:53 +0000

    Implement abstract app class.

---
+++ app.py
@@ -0,0 +1,72 @@
+from abc import ABC, abstractmethod
+import speech_recognition as sr
+
+# Abstract class that defines the methods amd attributes of Braille Apps. 
+class App(ABC):
+
+    
+
+    @abstractmethod
+    def on_start(self):
+        # Actions that an app wants to perform on app start
+        
+    @abstractmethod
+    def on_quit(self):
+        # Actions that an app wants to perform when quitting the app
+
+
+
+    def load_state(self, state):
+        # Rehydrate app state from local file system
+
+    
+    def save_state(self, state):
+        # Save app state to local file system
+
+
+    def recognize_speech_from_mic(recognizer, microphone):
+        """Transcribe speech from recorded from `microphone`.
+
+        Returns a dictionary with three keys:
+        "success": a boolean indicating whether or not the API request was
+                successful
+        "error":   `None` if no error occurred, otherwise a string containing
+                an error message if the API could not be reached or
+                speech was unrecognizable
+        "transcription": `None` if speech could not be transcribed,
+                otherwise a string containing the transcribed text
+        """
+        # check that recognizer and microphone arguments are appropriate type
+        if not isinstance(recognizer, sr.Recognizer):
+            raise TypeError("`recognizer` must be `Recognizer` instance")
+
+        if not isinstance(microphone, sr.Microphone):
+            raise TypeError("`microphone` must be `Microphone` instance")
+
+        # adjust the recognizer sensitivity to ambient noise and record audio
+        # from the microphone
+        with microphone as source:
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
+
+        # set up the response object
+        response = {
+            "success": True,
+            "error": None,
+            "transcription": None
+        }
+
+        # try recognizing the speech in the recording
+        # if a RequestError or UnknownValueError exception is caught,
+        #     update the response object accordingly
+        try:
+            response["transcription"] = recognizer.recognize_google(audio)
+        except sr.RequestError:
+            # API was unreachable or unresponsive
+            response["success"] = False
+            response["error"] = "API unavailable"
+        except sr.UnknownValueError:
+            # speech was unintelligible
+            response["error"] = "Unable to recognize speech"
+
+        return response
