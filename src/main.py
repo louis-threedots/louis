cba07a0d5f92f6fa850b9a84f26c5a6e36af5960
Ragnor Comerford <hello@ragnor.co>
2020-02-07 14:11:13 +0000

    Refactor and fix audio.

--- src/main.py
+++ src/main.py
@@ -2,28 +2,8 @@
 import time
 import speech_recognition as sr
 from audio import Audio
+import os
 
-# this is called from the background thread
-def callback(recognizer, audio):
-    print("Processing audio input ...")
-    # received audio data, now we'll recognize it using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        speech = recognizer.recognize_google(audio)
-        print("You said " + speech )
-        before_keyword, keyword, app_name = speech.partition("open")
-        if (app_name != ""):
-            print("Opening the app"+app_name)
-        
-
-    except sr.UnknownValueError:
-        print("Could not understand audio.")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-    #TODO: Listen for `open` and `close` to open appropriate apps.
 
 
 
@@ -31,8 +11,18 @@ def callback(recognizer, audio):
 def main():
     print("Louis has started. Listening for voice commands ...")
     audio = Audio()
-    audio.start_background_listening(callback)
-    while (True): time.sleep(1)
+    #time.sleep(6)
+    audio.speak("Welcome to Louis the brailliant assistant. You can now open any application using voice commands.") 
+   
+    while (True):
+        print("Listening")
+        response = audio.recognize_speech()
+        if response["transcription"] is not None:
+            before_keyword, keyword, app_name = response["transcription"].partition("open")
+            if (app_name != ""):
+                app_name = app_name.replace(" ","")
+                audio.speak("Opening the application"+app_name)
+
 
 if __name__ == "__main__":
     main()
