812d53ff67aec98f602cfc2a92df9671ed06a48b
Nina Becx <s1658172@capriati.inf.ed.ac.uk>
2020-02-21 18:08:02 +0000

    still work with lego

--- demos/tests.py
+++ demos/tests.py
@@ -3,6 +3,7 @@ import time
 start = time.time()
 import characters
 from cell import *
+from arduino import *
 
 # TO RUN ON EV3:
 #1
@@ -23,19 +24,16 @@ from cell import *
 
 c = Cell(1, Arduino())
 
-if not (c.motor.connected):
-        print ('Plug a motor into port A')
-else:
-        start_tests = time.time()
+start_tests = time.time()
 
-        for letter in "abcdefghijklmnopqrstuvwxyz":
-                print('Ready for next letter')
-                while not c.button.is_pressed:
-                        pass
-                c.rotate(letter)
-                print("\n :) !!Printed letter: ", letter, "\n",
-                    characters.characters[letter], "\n")
+for letter in "abcdefghijklmnopqrstuvwxyz":
+        print('Ready for next letter')
+        while not c.button.is_pressed:
+                pass
+        c.rotate(letter)
+        print("\n :) !!Printed letter: ", letter, "\n",
+            characters.characters[letter], "\n")
 
-        end = time.time()
-        print("\nTime program:" + str(end - start_tests))
-        print("Time total:" + str(end - start))
+end = time.time()
+print("\nTime program:" + str(end - start_tests))
+print("Time total:" + str(end - start))
--- src/arduino.py
+++ src/arduino.py
@@ -1,9 +1,13 @@
 
 import serial
+import ev3dev.ev3 as ev3
+import time
+
 class Arduino:
 
-    def __init__:
-        self.ser = serial.Serial('/dev/ttyACM0', 9600)
+    def __init__(self):
+        # self.ser = serial.Serial('/dev/ttyACM0', 9600)
+        self.motor = ev3.LargeMotor('outA')
 
     def discover(self):
         # [cell number, command, data1, data2]
@@ -11,8 +15,8 @@ class Arduino:
         self.ser.write(bytearray([255,0,0,1]), 'utf-8')
         while True :
             try:
-        state=ser.read(4)
-        print(state)
+                state=ser.read(4)
+                print(state)
             except:
                 pass
 
@@ -22,6 +26,11 @@ class Arduino:
 
     def run_to_rel_pos(self, rel_angle, cell_index):
         if rel_angle >= 0:
-            self.ser.write(bytearray([cell_index,1,0,0]), 'utf-8')
+            self.motor.run_to_rel_pos(position_sp = rel_angle, speed_sp = 250, stop_action = 'hold', ramp_up_sp = 0, ramp_down_sp = 150)
+            # self.ser.write(bytearray([cell_index,1,0,0]), 'utf-8')
         else:
-            self.ser.write(bytearray([cell_index,2,0,0]), 'utf-8')
+            self.motor.run_to_rel_pos(position_sp = rel_angle, speed_sp = 250, stop_action = 'hold', ramp_up_sp = 0, ramp_down_sp = 150)
+            # self.ser.write(bytearray([cell_index,2,0,0]), 'utf-8')
+
+        self.motor.wait_until('holding')
+        time.sleep(0.4)
